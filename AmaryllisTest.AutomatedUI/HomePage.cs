8b35efe7598717b5bbc105746e4c69fc5945912f
YoGa-jpg <antondavidovich45@gmail.com>
2022-03-30 22:40:07 +0300

    Fix filter test/swtiching to page-object

---
+++ AmaryllisTest.AutomatedUI/HomePage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace AmaryllisTest.AutomatedUI
+{
+    class HomePage
+    {
+        private readonly IWebDriver _driver;
+
+        private const string URI = "https://www.booking.com/";
+
+        private IWebElement CurrentLanguageMeta => _driver.FindElement(By.CssSelector("meta[property='og:locale']"));
+        private IWebElement CurrentLanguageElement => _driver.FindElement(By.CssSelector(".bui-avatar__image"));
+        private IWebElement SwedenLanguageElement => _driver.FindElement(
+            By.CssSelector(".bui-group__item:nth-child(4) .bui-grid__column:nth-child(3) .bui-inline-container__main"));
+        private IWebElement TicketsElement =>
+            _driver.FindElement(By.CssSelector(".bui-tab__item:nth-child(2) .bui-tab__text:nth-child(2)"));
+        private IWebElement LocationFilterElement => _driver.FindElement(By.Id("ss"));
+        private IWebElement DatesElement =>
+            _driver.FindElement(By.CssSelector(".xp__input-group:nth-child(2) .sb-date-field__icon"));
+        private IWebElement StartDateElement => _driver.FindElement(By.CssSelector(
+            ".bui-calendar__wrapper:nth-child(2) .bui-calendar__row:nth-child(2) > .bui-calendar__date:nth-child(4)"));
+        private IWebElement EndDateElement => _driver.FindElement(By.CssSelector(
+            ".bui-calendar__wrapper:nth-child(2) .bui-calendar__row:nth-child(2) > .bui-calendar__date:nth-child(6) > span > span"));
+        private IWebElement GuestsElement => _driver.FindElement(By.CssSelector(".xp__guests__count"));
+        private IWebElement ChildrenCountElement => _driver.FindElement(By.CssSelector(
+            "#xp__guests__inputs-container > div > div > div.sb-group__field.sb-group-children > div > div.bui-stepper__wrapper.sb-group__stepper-a11y > span.bui-stepper__display"));
+        private IWebElement SubtractChildrenElement => _driver.FindElement(By.CssSelector(
+            "#xp__guests__inputs-container > div > div > div.sb-group__field.sb-group-children > div > div.bui-stepper__wrapper.sb-group__stepper-a11y > button.bui-button.bui-button--secondary.bui-stepper__subtract-button"));
+        private IWebElement AddChildrenElement => _driver.FindElement(By.CssSelector(
+            "#xp__guests__inputs-container > div > div > div.sb-group__field.sb-group-children > div > div.bui-stepper__wrapper.sb-group__stepper-a11y > button.bui-button.bui-button--secondary.bui-stepper__add-button"));
+        private IWebElement ChildrenAgeElement => _driver.FindElement(By.Name("age"));
+        private IWebElement Children12 => _driver.FindElement(By.CssSelector("#xp__guests__inputs-container > div > div > div.sb-group__children__field.clearfix > select > option:nth-child(12)"));
+        private IWebElement SearchBoxElement =>
+            _driver.FindElement(By.CssSelector(".sb-searchbox__button > span:nth-child(1)"));
+
+        public string Language => CurrentLanguageMeta.GetAttribute("content");
+        public string Url => _driver.Url;
+        public int Children => int.Parse(ChildrenCountElement.Text);
+    }
+}
--- AmaryllisTest.AutomatedUI/UITest.cs
+++ AmaryllisTest.AutomatedUI/UITest.cs
@@ -26,10 +26,10 @@ namespace AmaryllisTest.AutomatedUI
         {
             _driver.Navigate().GoToUrl("https://www.booking.com");
 
-            _driver.FindElement(By.CssSelector(".bui-avatar__image")).Click();
-            _driver.FindElement(By.CssSelector(".bui-group__item:nth-child(4) .bui-grid__column:nth-child(3) .bui-inline-container__main")).Click();
+            _driver.FindElement(By.CssSelector(".bui-avatar__image")).Click(); // currentlanguage 
+            _driver.FindElement(By.CssSelector(".bui-group__item:nth-child(4) .bui-grid__column:nth-child(3) .bui-inline-container__main")).Click(); // sweden
 
-            Assert.AreEqual("sv_SE", _driver.FindElement(By.CssSelector("meta[property='og:locale']")).GetAttribute("content"));
+            Assert.AreEqual("sv_SE", _driver.FindElement(By.CssSelector("meta[property='og:locale']")).GetAttribute("content")); // language
         }
 
         [Test]
@@ -37,9 +37,9 @@ namespace AmaryllisTest.AutomatedUI
         {
             _driver.Navigate().GoToUrl("https://www.booking.com");
 
-            _driver.FindElement(By.CssSelector(".bui-tab__item:nth-child(2) .bui-tab__text:nth-child(2)")).Click();
+            _driver.FindElement(By.CssSelector(".bui-tab__item:nth-child(2) .bui-tab__text:nth-child(2)")).Click(); //tickets
 
-            var url = _driver.Url;
+            var url = _driver.Url; // Url
 
             Assert.AreNotEqual(url, "https://www.booking.com/tickets");
         }
@@ -70,6 +70,46 @@ namespace AmaryllisTest.AutomatedUI
 
             Assert.Pass();
             //wacakh@fexbox.org
+        }
+
+        [Test]
+        public void Add_Filters_List()
+        {
+            _driver.Navigate().GoToUrl("https://www.booking.com");
+            _driver.Manage().Window.Size = new System.Drawing.Size(945, 1020);
+
+            _driver.FindElement(By.Id("ss")).Click(); // locationfilter
+            _driver.FindElement(By.Id("ss")).SendKeys("Варшава");
+                //_driver.FindElement(By.CssSelector(".sb-autocomplete__item--icon_revamp:nth-child(1)")).Click();
+            _driver.FindElement(By.CssSelector(".xp__input-group:nth-child(2) .sb-date-field__icon")).Click(); // dateselement
+            _driver.FindElement(By.CssSelector(".bui-calendar__wrapper:nth-child(2) .bui-calendar__row:nth-child(2) > .bui-calendar__date:nth-child(4)")).Click(); //startdate element
+            _driver.FindElement(By.CssSelector(".bui-calendar__wrapper:nth-child(2) .bui-calendar__row:nth-child(2) > .bui-calendar__date:nth-child(6) > span > span")).Click(); //enddate element
+            _driver.FindElement(By.CssSelector(".xp__guests__count")).Click();//guestselement
+            //_driver.FindElement(By.CssSelector(".sb-group-children .bui-stepper__add-button")).Click();
+            //_driver.FindElement(By.Id("group_children")).SendKeys("1");
+
+            {
+                var count = int.Parse(_driver.FindElement(By.CssSelector(
+                        "#xp__guests__inputs-container > div > div > div.sb-group__field.sb-group-children > div > div.bui-stepper__wrapper.sb-group__stepper-a11y > span.bui-stepper__display"))
+                    .Text); // children
+
+                for (int i = count; i != 1; i = count > 1 ? i - 1 : i + 1)
+                {
+                    _driver.FindElement(By.CssSelector(
+                            i > 1
+                                ? "#xp__guests__inputs-container > div > div > div.sb-group__field.sb-group-children > div > div.bui-stepper__wrapper.sb-group__stepper-a11y > button.bui-button.bui-button--secondary.bui-stepper__subtract-button" //subtract
+                                : "#xp__guests__inputs-container > div > div > div.sb-group__field.sb-group-children > div > div.bui-stepper__wrapper.sb-group__stepper-a11y > button.bui-button.bui-button--secondary.bui-stepper__add-button")) //add
+                        .Click();
+                }
+            }
+
+            _driver.FindElement(By.Name("age")).Click();//children
+            {
+                var dropdown = _driver.FindElement(By.Name("age"));//children
+                dropdown.FindElement(By.CssSelector("#xp__guests__inputs-container > div > div > div.sb-group__children__field.clearfix > select > option:nth-child(12)")).Click();//children12
+            }
+            _driver.FindElement(By.CssSelector(".sb-searchbox__button > span:nth-child(1)")).Click();//searchbox
+            Assert.True(true);
         }
 
         public void Dispose()
