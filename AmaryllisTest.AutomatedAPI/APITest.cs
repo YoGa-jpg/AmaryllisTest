35996a75d0780e2aae079327932699001bdbaea4
YoGa-jpg <antondavidovich45@gmail.com>
2022-04-05 17:04:54 +0300

    Refactoring

--- AmaryllisTest.AutomatedAPI/APITest.cs
+++ AmaryllisTest.AutomatedAPI/APITest.cs
@@ -11,21 +11,9 @@ namespace AmaryllisTest.AutomatedAPI
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public async Task Get_Locations_ContainsMinsk()
         {
-            //List<Location> loaded;
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    var response = await client.GetAsync("https://www.metaweather.com/api/location/search/?query=min");
-            //    loaded = JsonSerializer.Deserialize<List<Location>>(await response.Content.ReadAsStringAsync());
-            //}
-            //Assert.True(loaded.Any(loc => loc.Title == "Minsk"));
             Assert.IsNotNull(await GetLocation("min", "Minsk"));
         }
 
@@ -33,12 +21,6 @@ namespace AmaryllisTest.AutomatedAPI
         public async Task Check_LattitudeLongitude()
         {
             Location city = await GetLocation("min", "Minsk");
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    var response = await client.GetAsync("https://www.metaweather.com/api/location/search/?query=min");
-            //    city = JsonSerializer.Deserialize<List<Location>>(await response.Content.ReadAsStringAsync())
-            //        .Single(loc => loc.Title == "Minsk");
-            //}
 
             Assert.AreEqual(city.LattLong, "53.90255,27.563101");
         }
@@ -48,17 +30,7 @@ namespace AmaryllisTest.AutomatedAPI
         {
             Location city = await GetLocation("min", "Minsk");
             LocationInfo locationInfo = await GetLocationInfo(city);
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    var response = await client.GetAsync("https://www.metaweather.com/api/location/search/?query=min");
-            //    city = JsonSerializer.Deserialize<List<Location>>(await response.Content.ReadAsStringAsync())
-            //        .Single(loc => loc.Title == "Minsk");
 
-            //    response = await client.GetAsync($"https://www.metaweather.com/api/location/{city.Woeid}");
-            //    var locationInfo = JsonSerializer.Deserialize<LocationInfo>(await response.Content.ReadAsStringAsync());
-
-            //    Assert.IsNotNull(locationInfo.ConsolidatedWeather.First());
-            //}
             Assert.IsNotNull(locationInfo.ConsolidatedWeather.First());
         }
 
@@ -67,17 +39,7 @@ namespace AmaryllisTest.AutomatedAPI
         {
             Location city = await GetLocation("min", "Minsk");
             LocationInfo locationInfo = await GetLocationInfo(city);
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    var response = await client.GetAsync("https://www.metaweather.com/api/location/search/?query=min");
-            //    city = JsonSerializer.Deserialize<List<Location>>(await response.Content.ReadAsStringAsync())
-            //        .Single(loc => loc.Title == "Minsk");
 
-            //    response = await client.GetAsync($"https://www.metaweather.com/api/location/{city.Woeid}");
-            //    var locationInfo = JsonSerializer.Deserialize<LocationInfo>(await response.Content.ReadAsStringAsync());
-
-            //    Assert.True(locationInfo.ConsolidatedWeather.All(weather => weather.MinTemp > -5 & weather.MaxTemp < 20));
-            //}
             Assert.True(locationInfo.ConsolidatedWeather.All(weather => weather.MinTemp > -5 & weather.MaxTemp < 20));
         }
 
@@ -89,22 +51,7 @@ namespace AmaryllisTest.AutomatedAPI
             LocationInfo locationInfo = await GetLocationInfo(city);
             List<ConsolidatedWeather> weathers = await GetByDate(currentDate.Year - 5, currentDate.Month, currentDate.Day,
                 city);
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    var response = await client.GetAsync("https://www.metaweather.com/api/location/search/?query=min");
-            //    city = await GetLocation("min", "Minsk");
-            //    //city = JsonSerializer.Deserialize<List<Location>>(await response.Content.ReadAsStringAsync())
-            //    //    .Single(loc => loc.Title == "Minsk");
 
-            //    //response = await client.GetAsync($"https://www.metaweather.com/api/location/{city.Woeid}");
-            //    //var locationInfo = JsonSerializer.Deserialize<LocationInfo>(await response.Content.ReadAsStringAsync());
-            //    var locationInfo = await GetLocationInfo(city);
-
-            //    response = await client.GetAsync($"https://www.metaweather.com/api/location/{city.Woeid}/{currentDate.Year - 5}/{currentDate.Month}/{currentDate.Day}");
-            //    var weathers = JsonSerializer.Deserialize<List<ConsolidatedWeather>>(await response.Content.ReadAsStringAsync());
-
-            //    Assert.True(weathers.Any(weather => weather.WeatherStateName == locationInfo.ConsolidatedWeather.First().WeatherStateName));
-            //}
             Assert.True(weathers.Any(weather => weather.WeatherStateName == locationInfo.ConsolidatedWeather.First().WeatherStateName));
         }
 
